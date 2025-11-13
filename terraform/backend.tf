26a23b4ee19148ef3d0a9639fd905dd568398ca2
tlkone <tainy.kone@gmail.com>
2025-11-13 17:54:24 -0600

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage88kt79"
+    resource_group_name  = "<The resource group containing your state file"
+    storage_account_name = "<The name of the storage account containing your state file"
     container_name       = "tfstate"
-    key                  = "landingzone/infratk.tfstate"
+    key                  = "<your tfstate file>"
   }
 }
